2ffa81599c69f35068ddd8220697e67a492003dd
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-03 21:01:18 +0100

    feat(model) Create restaurant model and migrations to database

--- src/restaurants_project/restaurants_api/models.py
+++ src/restaurants_project/restaurants_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class RestaurantItem(models.Model):
+    """This is the restaurant model."""
+
+    name = models.CharField(max_length=255, unique=True)
+    opens_at = models.DateTimeField(auto_now_add=True)
+    closes_at = models.DateTimeField(auto_now_add=True)
+
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        """Return the model as a string."""
+
+        return self.name

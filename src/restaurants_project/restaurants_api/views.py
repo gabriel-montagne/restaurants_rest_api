281135f75816ef81c916884fcb27b5a9f8ace20e
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-04 10:36:06 +0100

    feat(view) Add RestaurantItemViewSet

--- src/restaurants_project/restaurants_api/views.py
+++ src/restaurants_project/restaurants_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+from . import models
+
 # Create your views here.
+
+class RestaurantItemViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading, updating and deleting restaurant items."""
+
+    serializer_class = serializers.RestaurantItemSerializer
+    queryset = models.RestaurantItem.objects.all()
